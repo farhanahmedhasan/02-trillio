f17eb8b246255acb0a916ad17c2ebf1778d57a50
Hasan <aaxhasandjmamunxyz2015@gmail.com>
2021-07-13 17:01:22 +0600

    Added overView

--- src/components/Hotel.js
+++ src/components/Hotel.js
@@ -1,7 +1,53 @@
 import React from 'react';
+import Hotel1 from '../assets/img/hotel-1.jpg';
+import Hotel2 from '../assets/img/hotel-2.jpg';
+import Hotel3 from '../assets/img/hotel-3.jpg';
+
+import { AiFillStar } from 'react-icons/ai';
+import { FiMapPin } from 'react-icons/fi';
+
+import '../styles/components/_hotel.scss';
 
 const Hotel = () => {
-  return <main className='hotelView'>Hotel view</main>;
+  return (
+    <main className='hotelView'>
+      <div className='gallery'>
+        <figcaption className='gallery__item'>
+          <img src={Hotel1} alt='hotel' className='gallery__photo' />
+        </figcaption>
+        <figcaption className='gallery__item'>
+          <img src={Hotel2} alt='hotel' className='gallery__photo' />
+        </figcaption>
+        <figcaption className='gallery__item'>
+          <img src={Hotel3} alt='hotel' className='gallery__photo' />
+        </figcaption>
+      </div>
+
+      <div className='overview'>
+        <div className='overview__heading'>
+          <h1>Hotel Las Pamas</h1>
+        </div>
+
+        <div className='overview__stars'>
+          <AiFillStar className='overview__icon overview__icon--star' />
+          <AiFillStar className='overview__icon overview__icon--star' />
+          <AiFillStar className='overview__icon overview__icon--star' />
+          <AiFillStar className='overview__icon overview__icon--star' />
+          <AiFillStar className='overview__icon overview__icon--star' />
+        </div>
+
+        <div className='overview__loaction'>
+          <FiMapPin className='overview__icon overview__icon--location' />
+          <button className='btn btn--inline btn--animatedPulsate'>Albuferia Portugal</button>
+        </div>
+
+        <div className='overview__rating'>
+          <div className='overview__ratingPoint'>8.7</div>
+          <div className='overview__ratingCount'>412 votes</div>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default Hotel;
