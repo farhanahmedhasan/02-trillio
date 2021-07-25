b5594c4d5deda9026e30dada96acaf77cb3dbed9
Hasan <aaxhasandjmamunxyz2015@gmail.com>
2021-07-25 23:04:06 +0600

    Fixed a Disebar Issue

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,27 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { sidebarData } from '../data';
 import { NavLink } from 'react-router-dom';
 
 const Sidebar = () => {
-  const [location, setLocation] = useState('');
-
-  const readPathname = (match, location) => {
-    if (match) {
-      setLocation(location.pathname);
-    }
-  };
-
   return (
     <nav className='sidebar'>
       <ul className='sidebar__links'>
         {sidebarData.map((item) => {
           const { id, icon, text, link } = item;
           return (
-            <li
-              key={id}
-              className={location === link ? 'sidebar__item sidebar__item--active' : 'sidebar__item'}
-            >
-              <NavLink to={link} className='sidebar__link' isActive={readPathname}>
+            <li key={id} className='sidebar__item '>
+              <NavLink to={link} exact={true} className='sidebar__link' activeClassName='sidebar__link--active'>
                 {icon}
                 <span>{text}</span>
               </NavLink>
